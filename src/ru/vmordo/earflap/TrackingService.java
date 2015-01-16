ed80d89cd96750f2a3fd79bce9a287ed5dfdc5b5
Кеша <@ASUS>
2015-01-16 09:09:03 +0700

    seekbar with listner

--- src/ru/vmordo/earflap/TrackingService.java
+++ src/ru/vmordo/earflap/TrackingService.java
@@ -7,7 +7,6 @@ import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.os.IBinder;
 import android.preference.PreferenceManager;
